2e93e2d1520c6ac38fa5b555ca0a72f5adf41cf9
JALDACO1 <jaldaco1@ucol.mx>
2023-10-03 23:24:22 -0600

    add. update

---
+++ index.js
@@ -0,0 +1,22 @@
+//declaracion de variables en javascript
+//var contador = 5;
+//contador = 8;
+//const constante = 4;
+//constante = 10;
+
+
+//alerta en js
+//alert('Hola desde una alerta desde javascript');
+//console.log('Esto es javascript desde el html');
+//console.log(numero);
+//console.log(constante);
+//concatenacion
+let nombre = 'Jorge Emiliano';
+let apellido =' Aldaco';
+console.log('Mi nombre es: '+nombre+apellido);
+
+//document.write('Mi nombre es: '+nombre+apellido)
+
+let seleccion = document.querySelector('#seccion1');
+
+console.log(seleccion);
